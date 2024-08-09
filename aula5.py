a956c20b425e4c5baae08d548b8ceccc9d04e76a
letlet12312 <leticia.maria.santos100@gmail.com>
2024-08-09 14:52:16 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,19 @@
+# for numero in range(2, 21, 2):
+#   print(numero)
+
+# for num in range(5, 55, 5):
+#   print(num)
+# multiplas_5=range(5, 55, 5)
+# soma= sum(multiplas_5)
+# print('a soma dos multiplos é:', soma)
+
+
+# variavel_inteiro=20
+# variavel_floante=3.5
+ 
+# print('o tipo da variavel inteiro é:', type(variavel_inteiro))
+# print('o tipo da variavel floante é:', type(variavel_floante))
+
+
+# nome= input('escreva seu nome>')
+# print(f' ola ,{nome}')
