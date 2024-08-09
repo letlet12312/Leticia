acd91c0d16156f730573b54c59023ffaac3eaea1
letlet12312 <leticia.maria.santos100@gmail.com>
2024-08-09 17:14:12 -0300

    Add files via upload

--- aula5.py
+++ aula5.py
@@ -17,3 +17,67 @@
 
 # nome= input('escreva seu nome>')
 # print(f' ola ,{nome}')
+# #   -10-9-8-7-6-5-4-3-2-1
+# lista = [1,2,3,4,5,6,7,8,9,10]
+#    0,1,2,3,4,5,6,7,8,9,
+# lista = list(range(1,21))
+# soma= sum(lista)
+# comprimento = len(lista)
+# maior = max(lista)
+# menor = min(lista)
+# print(f'''soma:', soma,'\ncomprimento: comprimento,'\n menor',menor)''')
+# print(f'''soma:{soma})
+# comprimento:{comprimento}
+# menor:{menor}
+# ''') 
+
+# lista = tuple(range(1,10))
+# print = lista
+# # ( star stop step by step)
+
+# # for n in range(1,10):
+# n1, n2, n3 = lista
+# print(n1, n2, n3)
+
+# conta=(4*6//2)
+# print(conta)
+
+# Soma = (5+7)
+# print(Soma)
+
+# conta=(4*6//2)
+# print(conta)
+
+# conta = (3**4)
+# print(conta)
+
+# Soma=(8+12+15//3)
+# print(Soma)
+
+# soma=(10-2)
+# soma=(soma*5)
+# print(soma)
+
+# div = (27//3+5)
+# print(div)
+
+# print (17%4)
+
+# quadrado=(9*3)
+# print(quadrado*quadrado)
+
+# raiz = 81
+# r = raiz**(1//2)
+# print(r)
+
+# conta = (3*4+20)
+# print(conta)
+
+# conta = (15*2-7)
+# print(conta)
+
+# print(5**3)
+
+# soma=(17+21+25)
+# print(soma//3)
+
